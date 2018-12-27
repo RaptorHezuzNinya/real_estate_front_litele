7eeebf170c844a9db99fd1f0fb5bb7e0c70e474b
RaptorHezuzNinya <w22243vrh@gmail.com>
2018-12-27 21:45:53 +0100

    better

--- src/lit-app.js
+++ src/lit-app.js
@@ -10,16 +10,11 @@ class LitApp extends LitElement {
 				${styles}
 			</style>
 			<div class="app">
-				<header class="app-header">
-					<img src="../assets/logo.svg" class="app-logo" alt="logo" />
-					<h1 class="app-title">Welcome to LitHTML</h1>
-				</header>
+	
+				<nav-bar></nav-bar>
 
 				<hello-world .greeting=${'Welcome'}></hello-world>
 				
-				<a aria-label="Create lit app on Github" href="https://github.com/thepassle/create-lit-app">
-					<img src="../assets/github.svg" class="app-gh" alt />
-				</a>
 			</div>
 		`;
 	}

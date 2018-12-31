93014168fbde9ba12cb0dea4742ad81c49a1a0da
RaptorHezuzNinya <w22243vrh@gmail.com>
2018-12-31 03:03:32 +0100

    new comp

---
+++ src/components/apiComponent/apiComponent.js
@@ -0,0 +1,39 @@
+import { LitElement } from '@polymer/lit-element';
+
+class ApiComponent extends LitElement {
+    static get properties() {
+        return {
+            apiRoute: {
+                type: String,
+                value: ''
+            }
+        };
+    }
+    constructor() {
+        super();
+        this.axios = require('axios');
+
+    }
+    
+    render() {
+        
+        this.axios.get('http://127.0.0.1:5000/tenants')
+            .then(function (response) {
+                // handle success
+                console.log(response);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function () {
+                // always executed
+            });
+
+    }
+}
+
+customElements.define('api-component', ApiComponent);
+
+
+

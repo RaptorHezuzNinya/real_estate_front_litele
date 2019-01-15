e815bd2f28a20a57dd73e32fba3a98dd8fa9065e
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-01-15 20:42:07 +0100

    update imports

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,4 +1,4 @@
-import { LitElement, html } from '@polymer/lit-element/';
+import { LitElement, html } from 'lit-element/';
 import { navBarStyles } from './navBarStyles.js';
 import '../selectBox/selectBox.js';
 import '../apiComponent/apiComponent.js';
